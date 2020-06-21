b77a439394bfb84d5ea491a9998eb9d8d9df3c45
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2020-06-20 23:30:57 -0300

    Realizado estudo das coleções em Expressions Language.

--- maratona-jsf/src/main/java/Maratona/bean/estudante/estudante/EstudanteRegistrarBean.java
+++ maratona-jsf/src/main/java/Maratona/bean/estudante/estudante/EstudanteRegistrarBean.java
@@ -4,6 +4,9 @@ import Maratona.bean.estudante.estudante.model.Estudante;
 
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 /**
  * Created by Allyson 20/06/2020
@@ -14,6 +17,55 @@ import java.io.Serializable;
 public class EstudanteRegistrarBean implements Serializable{
 
     private Estudante estudante = new Estudante();
+
+    private String [] nomesArray = {"Ana","Lobo","Vovo"};
+
+    private List<String> nomeList = asList ("Ana", "Lobo", "Vovo");
+
+    private Set<String> nomesSet = new HashSet<>(asList ("Ana", "Lobo", "Vovo"));
+
+    private Map<String , String> nomesMap = new HashMap<>();
+
+    {
+        nomesMap.put("Goku", "O mais forte");
+        nomesMap.put("One", "Primeiro");
+        nomesMap.put("Segundo", "dDoius");
+        for(String nome: nomesSet){
+            System.out.println(nome);
+        }
+    }
+
+    public Map<String, String> getNomesMap() {
+        return nomesMap;
+    }
+
+    public void setNomesMap(Map<String, String> nomesMap) {
+        this.nomesMap = nomesMap;
+    }
+
+    public Set<String> getNomesSet() {
+        return nomesSet;
+    }
+
+    public void setNomesSet(Set<String> nomesSet) {
+        this.nomesSet = nomesSet;
+    }
+
+    public List<String> getNomeList() {
+        return nomeList;
+    }
+
+    public void setNomeList(List<String> nomeList) {
+        this.nomeList = nomeList;
+    }
+
+    public String[] getNomesArray() {
+        return nomesArray;
+    }
+
+    public void setNomesArray(String[] nomesArray) {
+        this.nomesArray = nomesArray;
+    }
 
     public Estudante getEstudante() {
         return estudante;
